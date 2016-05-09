88af7297a1ad8b0c360d3ca49834f1ebe88baaab
Joshua Appelman <jappelman@xebia.com>
2016-05-09 21:15:26 +0200

    Adds challenge 18.

---
+++ 18/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import fileinput
+from math import ceil
+
+for line in fileinput.input():
+    low, base = map(int, line.strip().split(','))
+    print(base * ceil(low // base))
+

9c9d98212babf0920381f6c6ed28064de06ad00d
Joshua Boy Nicolai Appelman <joshua@jbna.nl>
2016-05-24 22:31:56 +0200

    Adds solution 240.

---
+++ 240/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import fileinput
+
+primes = [ 3, 7, 31, 127 ]
+
+for line in fileinput.input():
+    n = int(line)
+    print(', '.join(str(prime) for prime in primes if prime < n))

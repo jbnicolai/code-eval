d3e0696d7d43eda87b0781e04214c3d8f9974a37
Joshua Appelman <jappelman@xebia.com>
2016-05-25 17:12:21 +0200

    Adds solution 115.

--- 115/main.py
+++ 115/main.py
@@ -4,4 +4,9 @@ import fileinput
 for line in fileinput.input():
     nums = [ word for word in line.strip().split(',') if word.isdigit() ]
     words = [ word for word in line.strip().split(',')  if not word.isdigit() ]
-    print('|'.join([','.join(words), ','.join(nums)]))
+    output = []
+    if words:
+        output.append(','.join(words))
+    if nums:
+        output.append(','.join(nums))
+    print('|'.join(output))
